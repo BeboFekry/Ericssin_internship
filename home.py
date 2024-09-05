eb4c39ab2d529799a7b4f126fc7da0e2facfe193
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-09-06 01:14:56 +0300

    Update home.py

--- home.py
+++ home.py
@@ -14,6 +14,6 @@ with col1:
 with col2:
     st.write("Task 2: Text2Pandas Engine using LLM")
     st.write("  \n")
-    st.image("images/Chat bot-pana.png", caption="Task 2: Text2Pandas Engine using LLM")
+    st.image("images/Chat bot-pana.png", caption="Takes human query and return the suitable pandas code)
     st.page_link(page2, use_container_width=1)
 st.write("---")
